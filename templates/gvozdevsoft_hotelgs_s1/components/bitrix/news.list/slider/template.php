efd388768b065d5cc418ca584298cbc4ca9348b5
alekslesik <alekslesik@gmail.com>
2024-01-16 15:36:32 +0300

    Revert "binovo widget"
    This reverts commit dd8f2dfcc96530e7ca37e8a49c2f3a1a3c7869e4.

--- templates/gvozdevsoft_hotelgs_s1/components/bitrix/news.list/slider/template.php
+++ templates/gvozdevsoft_hotelgs_s1/components/bitrix/news.list/slider/template.php
@@ -58,9 +58,58 @@ $this->setFrameMode(true);
 				</div>
 				</div>
 			<? endforeach; ?>
-
 			</div>
+
 
 			<div class="left" id="_bn_widget_">
 				<a href="https://bnovo.ru/" id="_bnovo_link_" target="_blank">Bnovo</a>
 			</div>
+			<script src="//widget.reservationsteps.ru/js/bnovo.js"></script>
+			<script type="text/javascript">
+				(function() {
+					Bnovo_Widget.init(function() {
+						Bnovo_Widget.open('_bn_widget_', {
+							type: "horizontal",
+							uid: "123e4567-e89b-12d3-a456-426655440000",
+							lang: "ru",
+							currency: "RUB",
+							width: "100%",
+							width_mobile: "300",
+							background: "#ffffff",
+							background_mobile: "#ffffff",
+							bg_alpha: "100",
+							bg_alpha_mobile: "100",
+							border_color_mobile: "#C6CAD3",
+							padding: "24",
+							padding_mobile: "24",
+							border_radius: "8",
+							button_font_size: "14",
+							button_height: "42",
+							font_type: "inter",
+							title_color: "#242742",
+							title_color_mobile: "#242742",
+							title_size: "22",
+							title_size_mobile: "22",
+							inp_color: "#242742",
+							inp_bordhover: "#dedfe3",
+							inp_bordcolor: "#BCBCBC",
+							inp_alpha: "100",
+							btn_background: "#1875F0",
+							btn_background_over: "#1850D2",
+							btn_textcolor: "#FFFFFF",
+							btn_textover: "#FFFFFF",
+							btn_bordcolor: "#1875F0",
+							btn_bordhover: "#1850D2",
+							adults_default: "1",
+							dates_preset: "on",
+							dfrom_today: "on",
+							dfrom_value: "2",
+							dto_nextday: "on",
+							dto_value: "2",
+							cancel_color: "#FFFFFF",
+							url: "http://nika-hotel.devbx.ru/",
+							switch_mobiles_width: "800",
+						});
+					});
+				})();
+			</script>
