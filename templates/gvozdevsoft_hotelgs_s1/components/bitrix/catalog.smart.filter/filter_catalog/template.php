0f23d3eadf8e03fdd1cae664ff0a4565c5082a9b
alekslesik <alekslesik@gmail.com>
2024-04-01 13:35:57 +0200

    hide filter on catalog list page

--- templates/gvozdevsoft_hotelgs_s1/components/bitrix/catalog.smart.filter/filter_catalog/template.php
+++ templates/gvozdevsoft_hotelgs_s1/components/bitrix/catalog.smart.filter/filter_catalog/template.php
@@ -21,60 +21,6 @@ $templateData = array(
 <div class="bx_filter <?= $templateData["TEMPLATE_CLASS"] ?> catalog-filter" id="catalog-filter">
 	<div class="bx_filter_section catalog-filter__inner">
 
-		<div class="left" id="_bn_widget_">
-			<a href="https://bnovo.ru/" id="_bnovo_link_" target="_blank"></a>
-		</div>
-		<script src="//widget.reservationsteps.ru/js/bnovo.js"></script>
-		<script type="text/javascript">
-			(function() {
-				Bnovo_Widget.init(function() {
-					Bnovo_Widget.open('_bn_widget_', {
-						type: "vertical",
-						uid: "1d61c305-1caa-4f27-9ca4-a7d836d0db34",
-						lang: "ru",
-						width: "250",
-						width_mobile: "250",
-						background: "#ffffff",
-						background_mobile: "#ffffff",
-						bg_alpha: "100",
-						bg_alpha_mobile: "100",
-						border_color_mobile: "#C6CAD3",
-						padding: "10",
-						padding_mobile: "24",
-						border_radius: "8",
-						button_font_size: "14",
-						button_height: "42",
-						font_type: "inter",
-						title: "Бронирование номеров. Гарантия заселения",
-						title_color: "#242742",
-						title_color_mobile: "#242742",
-						title_size: "19",
-						title_size_mobile: "22",
-						inp_color: "#242742",
-						inp_bordhover: "#dedfe3",
-						inp_bordcolor: "#BCBCBC",
-						inp_alpha: "100",
-						btn_background: "#6495ed",
-						btn_background_over: "#1850D2",
-						btn_textcolor: "#FFFFFF",
-						btn_textover: "#FFFFFF",
-						btn_bordcolor: "#6495ed",
-						btn_bordhover: "#1850D2",
-						min_age: "0",
-						max_age: "17",
-						adults_default: "1",
-						dates_preset: "on",
-						dfrom_today: "on",
-						dfrom_value: "2",
-						dto_nextday: "on",
-						dto_value: "2",
-						cancel_color: "#FFFFFF",
-						url: "https://clevermetrics.ru/wubook/",
-						switch_mobiles_width: "800",
-					});
-				});
-			})();
-		</script>
 
 		<div class="bx_filter_title catalog-filter__title"><? echo GetMessage("CT_BCSF_FILTER_TITLE") ?></div>
 		<form name="<? echo $arResult["FILTER_NAME"] . "_form" ?>" action="<? echo $arResult["FORM_ACTION"] ?>" method="get" class="smartfilter catalog-filter__box">
